c064e07e958c76407e72faadd64dca5c7d8aaa3a
lizrocha <vanessa.rocha@xpertal.com>
2022-02-28 09:52:17 -0600

    Se agrega funcionalidad de actualizar, con excepciones y se agrego una excepcion en agregar

--- proyecto_agencia/src/main/java/com/example/demo/dto/flightsDTOS/FlightsDTO.java
+++ proyecto_agencia/src/main/java/com/example/demo/dto/flightsDTOS/FlightsDTO.java
@@ -1,54 +1,39 @@
-package com.example.demo.model;
+package com.example.demo.dto.flightsDTOS;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
-import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@Entity
-@Table(name="flights")
-public class Flights {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
-    private int id_vuelo;
-
-    @Column(unique = true)
-    @NotBlank(message = "El campo 'flightNumber' no debe estar vacio")
-    //@Unique(message = "El campo 'flightNumber' se esta duplicando")
-    private String flightNumber;
-
+@Data
+public class FlightsDTO {
+    @NotNull(message = "Se debe ingresar el campo 'name'")
     @NotBlank(message = "El campo 'name' no debe estar vacio")
     private String name;
-
+    @NotNull(message = "Se debe ingresar el campo 'origin'")
     @NotBlank(message = "El campo 'origin' no debe estar vacio")
     private String origin;
-
+    @NotNull(message = "Se debe ingresar el campo 'destination'")
     @NotBlank(message = "El campo 'destination' no debe estar vacio")
     private String destination;
-
+    @NotNull(message = "Se debe ingresar el campo 'seatType'")
     @NotBlank(message = "El campo 'seatType' no debe estar vacio")
     private String seatType;
-
-    @NotNull(message = "El campo 'flightPrice' no debe estar vacio")
+    @NotNull(message = "Se debe ingresar el campo 'flightPrice'")
     private double flightPrice;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
-    @Column(nullable = false)
     @NotNull(message = "Se debe ingresar el campo 'goingDate'")
     private Date goingDate;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
-    @Column(nullable = false)
     @NotNull(message = "Se debe ingresar el campo 'returnDate'")
     private Date returnDate;
 }
--- proyecto_agencia/src/main/java/com/example/demo/service/flight/VuelosService.java
+++ proyecto_agencia/src/main/java/com/example/demo/service/flight/VuelosService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service.flight;
 
 
+import com.example.demo.dto.flightsDTOS.FlightsDTO;
 import com.example.demo.dto.flightsDTOS.ResponseFlightsDTO;
+import com.example.demo.dto.flightsDTOS.ResponsePPDFlightDTO;
 import com.example.demo.model.Flights;
 
 import java.util.Date;
@@ -10,4 +12,5 @@ public interface VuelosService {
     public Iterable<Flights> findAll();
     public ResponseFlightsDTO getFlightsAvailable(Date dateFrom, Date dateTo, String origin, String destination);
     public Flights save(Flights flights);
+    public ResponsePPDFlightDTO update(FlightsDTO flights, String flightNumber);
 }
